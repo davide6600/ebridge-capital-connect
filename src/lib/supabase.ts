b2fe9ca84fe7bd9137d3dcaef72521f8fda14ec5
BioDek <39161933+davide6600@users.noreply.github.com>
2025-05-27 19:38:02 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.https://llatuyckpkfpojghmcdp.supabase.co
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsYXR1eWNrcGtmcG9qZ2htY2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNTUzMDAsImV4cCI6MjA2MzgzMTMwMH0.1wgK_crm8pE3Bz0E74QKM_8ZbFVk0SFJBcIihwDKgGA
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
